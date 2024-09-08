490ae188acbe48f478b48f229cfcc4ae7073f221
Mohnish <mohnish.a007@gmail.com>
2024-09-08 01:23:38 +0100

    operators Airthmatic operator in C++

---
+++ basic_refresher/operators/arthops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout << "Arithmetic operators" << endl;
+int num1,num2;
+cout << "Please enter first number: "<< endl;
+cin >> num1;
+cout << "Please enter second number: "<< endl;
+cin >> num2;
+
+cout << "Addition: " << num1 + num2 << endl;
+cout << "Subtraction: " << num1 - num2 << endl;
+cout << "Multiplication: " << num1 * num2 << endl;
+cout << "Division: " << num1 / num2 << endl;
+return 0;
+}
