e80152b34b041949ed0407fc4d11050e3ce25400
Mohnish <mohnish.a007@gmail.com>
2024-09-08 01:27:17 +0100

    operators Airthmatic operator special cases in C++

--- basic_refresher/operators/arthops.cpp
+++ basic_refresher/operators/arthops.cpp
@@ -12,5 +12,8 @@ cout << "Addition: " << num1 + num2 << endl;
 cout << "Subtraction: " << num1 - num2 << endl;
 cout << "Multiplication: " << num1 * num2 << endl;
 cout << "Division: " << num1 / num2 << endl;
+cout << "Modulo: " << num1 % num2 << endl;
+cout << "int division give int result not the float " << 5 / 2 << endl;
+cout << "int division give float if " << 5.5 / 2 << endl;
 return 0;
 }
