0b948e3455781db62eaa3d4c6f38c01ab6fa14f7
Mohnish <mohnish.a007@gmail.com>
2024-09-08 01:04:16 +0100

    variable C++ with explicit typecasting

---
+++ basic_refresher/variables/typeCasting.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // We can have implicit and explicit typecasting 
+    char grade = 'a';
+    cout << "Type casted value of grade "<< int(grade) << endl;
+    cout << "original value of grade "<< grade << endl;
+return 0;
+}
