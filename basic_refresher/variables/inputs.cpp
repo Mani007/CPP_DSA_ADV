f67f2fb56eedc744474bcfa72e20de6dc05f2ef2
Mohnish <mohnish.a007@gmail.com>
2024-09-08 01:12:41 +0100

    variable C++ with user input using cin>>

---
+++ basic_refresher/variables/inputs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout << "Running some user input in C++" << endl;
+int age ;
+cout << "Print some garbage value of variable age as it is not assigned any value is " << age << endl;
+
+cout << "Now Please, Enter user age: ";
+cin >> age;
+
+cout << "Your entered age is: " << age << endl;
+
+return 0;
+}
