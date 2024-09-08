95fd2764d361e63fa2f028036c1a301c1a6c2ad9
Mohnish <mohnish.a007@gmail.com>
2024-09-08 01:33:47 +0100

    operators relational operator  in C++

--- basic_refresher/operators/arthops.cpp
+++ basic_refresher/operators/arthops.cpp
@@ -15,5 +15,11 @@ cout << "Division: " << num1 / num2 << endl;
 cout << "Modulo: " << num1 % num2 << endl;
 cout << "int division give int result not the float " << 5 / 2 << endl;
 cout << "int division give float if " << 5.5 / 2 << endl;
+cout << "Relational operators for > " << (num1 > num2) << endl; // make sure you put brackets during operations returns 0 or 1 as false and true value respectively 
+cout << "Relational operators for >= " << (num1 >= num2) << endl; // make sure you put brackets during operations
+cout << "Relational operators for < " << (num1 < num2) << endl;
+cout << "Relational operators for <= " << (num1 <= num2) << endl;
+cout << "Relational operators for not equal " << (num1 != num2) << endl; 
+
 return 0;
 }
