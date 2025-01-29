6294345911e78f3c298831276b5d25e8b01de96d
Mohnish <mohnish.a007@gmail.com>
2025-01-29 23:45:11 +0000

    you cannot pass string by addressof operstor &str

--- basic_refresher/pointers/pbasic2.cpp
+++ basic_refresher/pointers/pbasic2.cpp
@@ -13,7 +13,7 @@ int b = 20;
 cout << "Giving a SUM function call below" << endl;
 ans = ADD(4,3); // parameter passed by value
 cout << "The ADD of 4 and 3 using function ADD function by value is: " << ans << endl;
-ansref = ADDREF(&a,&b,str); // parameter passed by value
+ansref = ADDREF(&a,&b,&str); // parameter passed by value
 cout << "The ADDREF of 4 and 3 using function ADDREF function by value is: " << ansref << endl;
     
 
@@ -23,7 +23,7 @@ return 0;
 int ADD(int a, int b) {
 return a + b;
 }
-int ADDREF(int *a, int *b, string slocal) {
-cout << "Local string by value is "<<slocal << endl;
+int ADDREF(int *a, int *b, string *slocal) {
+cout << "Local string by value is "<<*slocal << endl;
 return *a + *b;
 }
