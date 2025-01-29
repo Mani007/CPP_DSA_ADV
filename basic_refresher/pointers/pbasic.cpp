0e41a01e8b08da5c9d3f9367d09f05aa5f9db67a
Mohnish <mohnish.a007@gmail.com>
2025-01-29 22:00:50 +0000

    double and triple pointer not working

--- basic_refresher/pointers/pbasic.cpp
+++ basic_refresher/pointers/pbasic.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 int main() {
   int var1 = 43;
-  int *ptr;
-  ptr = &var1;
+  int *ptr1;
+  int **ptr2; // double pointer
+  int ***ptr3; // triple pointer
+  ptr1 = &var1;
+  ptr2 = &ptr1;
+  ptr3 = &ptr2;
   cout<<"value of var1 is " << var1<<endl;
   cout<<"address of var1 is " << &var1<<endl;
-  cout<<"address in pointer ptr is " << ptr<<endl;
-  cout<<"address OF pointer ptr is " << &ptr<<endl;
-  cout<<"value in pointer ptr is " << *ptr<<endl;
+  cout<<"address in pointer ptr1 is " << ptr1<<endl;
+  cout<<"address OF pointer ptr1 is " << &ptr1<<endl;
+  cout<<"value in pointer ptr1 is " << *ptr1<<endl;
+  cout<<"value in pointer ptr2 is " << **ptr1<<endl;
+  cout<<"value in pointer ptr3 is " << ***ptr1<<endl;
   cout<<"size of var1 is " << sizeof(var1)<<" Bytes"<<endl;
-  cout<<"size of ptr is " << sizeof(ptr) << " Bytes"<<endl;
+  cout<<"size of ptr is " << sizeof(ptr1) << " Bytes"<<endl;
+
 
   return 0;
 }
