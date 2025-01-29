b86a0fb77cbded113b2feef21e60ec9ae3272791
Mohnish <mohnish.a007@gmail.com>
2025-01-29 23:30:44 +0000

    simple function call by value and call by reference

--- basic_refresher/pointers/pbasic2.cpp
+++ basic_refresher/pointers/pbasic2.cpp
@@ -3,9 +3,14 @@ using namespace std;
 int ADD(int a, int b);
 int main() {
 int ans;
+int ansref;
+int a = 10;
+int b = 20;
 cout << "Giving a SUM function call below" << endl;
-ans = ADD(4,3); //
-cout << "The sum of 4 and 3 using function SUM is: " << ans << endl;
+ans = ADD(4,3); // parameter passed by value
+cout << "The ADD of 4 and 3 using function ADD function by value is: " << ans << endl;
+ansref = ADDREF(&a,&b); // parameter passed by value
+cout << "The ADD of 4 and 3 using function ADDREF function by value is: " << ansref << endl;
     
 
 return 0;
@@ -13,4 +18,7 @@ return 0;
 // Function definition
 int ADD(int a, int b) {
 return a + b;
+}
+int ADDREF(int *a, int *b) {
+return *a + *b;
 }
