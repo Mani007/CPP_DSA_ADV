c5f9d1a92492c9a6a4e6f32cdb0561675c994198
Mohnish <mohnish.a007@gmail.com>
2025-01-29 23:39:44 +0000

    simple function call by reference in details

--- basic_refresher/pointers/pbasic2.cpp
+++ basic_refresher/pointers/pbasic2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 int ADD(int a, int b);
+
+int ADDREF(int *a, int *b);
 int main() {
 int ans;
 int ansref;
@@ -10,7 +12,7 @@ cout << "Giving a SUM function call below" << endl;
 ans = ADD(4,3); // parameter passed by value
 cout << "The ADD of 4 and 3 using function ADD function by value is: " << ans << endl;
 ansref = ADDREF(&a,&b); // parameter passed by value
-cout << "The ADD of 4 and 3 using function ADDREF function by value is: " << ansref << endl;
+cout << "The ADDREF of 4 and 3 using function ADDREF function by value is: " << ansref << endl;
     
 
 return 0;
