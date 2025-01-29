01d721041e231a5867c9b5bc1952aa8578554c47
Mohnish <mohnish.a007@gmail.com>
2025-01-29 23:43:51 +0000

    passing string by value to the functions

--- basic_refresher/pointers/pbasic2.cpp
+++ basic_refresher/pointers/pbasic2.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int ADD(int a, int b);
 
-int ADDREF(int *a, int *b);
+int ADDREF(int *a, int *b,string slocal);
 int main() {
+string str = "YAAHAA";
 int ans;
 int ansref;
 int a = 10;
@@ -11,7 +13,7 @@ int b = 20;
 cout << "Giving a SUM function call below" << endl;
 ans = ADD(4,3); // parameter passed by value
 cout << "The ADD of 4 and 3 using function ADD function by value is: " << ans << endl;
-ansref = ADDREF(&a,&b); // parameter passed by value
+ansref = ADDREF(&a,&b,str); // parameter passed by value
 cout << "The ADDREF of 4 and 3 using function ADDREF function by value is: " << ansref << endl;
     
 
@@ -21,6 +23,7 @@ return 0;
 int ADD(int a, int b) {
 return a + b;
 }
-int ADDREF(int *a, int *b) {
+int ADDREF(int *a, int *b, string slocal) {
+cout << "Local string by value is "<<slocal << endl;
 return *a + *b;
 }
