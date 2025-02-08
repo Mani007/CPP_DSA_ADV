ae49df6fece5e1b3a39add72b9530166bc258497
Mohnish <mohnish.a007@gmail.com>
2025-02-08 22:58:18 +0000

    float pointer can store float values

--- basic_refresher/pointers/pbasics.cpp
+++ basic_refresher/pointers/pbasics.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main() {
     float f=10.3;
-    void *ptr;  // void pointer often used to store object
+    float *ptr;  // void pointer often used to store object
     ptr = &f;  // store address of float in void pointer
-    cout << " void pointer values" << *ptr <<endl;
-cout << "New file" << endl;
+    cout << " float pointer values " << *ptr <<endl;
+cout << " float pointer address " << ptr << endl;
 return 0;
 }
