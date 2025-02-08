887ef4465483ca46721cad6cfed1f214f83a446e
Mohnish <mohnish.a007@gmail.com>
2025-02-08 22:51:40 +0000

    void pointer need to store object address

--- basic_refresher/pointers/pbasics.cpp
+++ basic_refresher/pointers/pbasics.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
+    float f=10.3;
     void *ptr;  // void pointer often used to store object
+    ptr = &f;  // store address of float in void pointer
     cout << " void pointer values" << *ptr <<endl;
 cout << "New file" << endl;
 return 0;
