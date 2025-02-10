4166f3ed199bc1e9f90e7a3cb12a15afae2a1d1a
Mohnish <mohnish.a007@gmail.com>
2025-02-10 02:40:16 +0000

    pointer data type relation and importance

--- basic_refresher/pointers/pbasics.cpp
+++ basic_refresher/pointers/pbasics.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-    float f=10.3;
-    float *ptr;  // void pointer often used to store object
+    int f=43;
+    int *ptr;  // void pointer often used to store object
     ptr = &f;  // store address of float in void pointer
-    cout << " float pointer values " << *ptr <<endl;
-cout << " float pointer address " << ptr << endl;
+    cout << " int pointer values " << *ptr <<endl;
+    cout << " int pointer address " << ptr << endl;
+    cout << " int pointer VALUE type casted to float " << *(float* )ptr << endl;
+    cout << " int pointer ADDRESS type casted to float " << (float* )ptr << endl;
+    // Please note that the pointer pointing to the same location but dereferencing two different types of values 
+    
 return 0;
 }
