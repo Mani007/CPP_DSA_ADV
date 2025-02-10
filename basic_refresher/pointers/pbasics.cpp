14844f6720fbb45331af6405c56af5b66076f947
Mohnish <mohnish.a007@gmail.com>
2025-02-10 02:59:32 +0000

    int  pointer address increment and printing some garbage values

--- basic_refresher/pointers/pbasics.cpp
+++ basic_refresher/pointers/pbasics.cpp
@@ -2,11 +2,15 @@
 #include <iomanip>
 using namespace std;
 int main() {
-    char c='A';
-    char *ptr;  // void pointer often used to store object
-    ptr = &c;  // store address of float in void pointer
-    cout << " char pointer values " << *ptr <<endl;
-    cout << " char pointer address   " << std::dec << ptr << endl;
+    int i=25;
+    int *ptr;  // void pointer often used to store object
+    ptr = &i;  // store address of float in void pointer
+    cout << " int pointer values " << *ptr <<endl;
+    cout << " int pointer address   "  << ptr << endl;
+    cout << " int pointer address increment to 1   "  << ptr+1 << endl;
+    cout << " int pointer increment to 1  and value is "  << *(ptr+1) << endl;
+    cout << " int pointer address increment to 2  "  << ptr+2 << endl;
+    cout << " int pointer address increment to 2  "  << *(ptr+2) << endl;
    
     
 return 0;
