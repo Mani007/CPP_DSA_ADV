c32905115fe53629d4fb249d9d11e0022e6adc74
Mohnish <mohnish.a007@gmail.com>
2025-02-10 02:42:27 +0000

    int pointert typecased to character

--- basic_refresher/pointers/pbasics.cpp
+++ basic_refresher/pointers/pbasics.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int f=0b100000;
+    int f=43;
     int *ptr;  // void pointer often used to store object
     ptr = &f;  // store address of float in void pointer
     cout << " int pointer values " << *ptr <<endl;
     cout << " int pointer address " << ptr << endl;
-    cout << " int pointer VALUE type casted to float " << *(float* )ptr << endl;
-    cout << " int pointer ADDRESS type casted to float " << (float* )ptr << endl;
+    cout << " int pointer VALUE type casted to float " << *(char* )ptr << endl;
+    cout << " int pointer ADDRESS type casted to float " << (char* )ptr << endl;
     // Please note that the pointer pointing to the same location but dereferencing two different types of values 
     
 return 0;
