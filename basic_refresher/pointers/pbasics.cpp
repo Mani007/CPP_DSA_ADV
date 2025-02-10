daa4cf2c7f83c142e34af7b8820257c1557a3a42
Mohnish <mohnish.a007@gmail.com>
2025-02-10 02:55:25 +0000

    char pointer address not getting visible properly

--- basic_refresher/pointers/pbasics.cpp
+++ basic_refresher/pointers/pbasics.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main() {
-    int f=43;
-    int *ptr;  // void pointer often used to store object
-    ptr = &f;  // store address of float in void pointer
-    cout << " int pointer values " << *ptr <<endl;
-    cout << " int pointer address " << ptr << endl;
-    cout << " int pointer VALUE type casted to float " << *(char* )ptr << endl;
-    cout << " int pointer ADDRESS type casted to float " << (char* )ptr << endl;
-    // Please note that the pointer pointing to the same location but dereferencing two different types of values 
+    char c='A';
+    char *ptr;  // void pointer often used to store object
+    ptr = &c;  // store address of float in void pointer
+    cout << " char pointer values " << *ptr <<endl;
+    cout << " char pointer address   " << std::dec << ptr << endl;
+   
     
 return 0;
 }
