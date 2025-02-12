00b44a5c0fdf0e9ecb189159ad7b434e028d0036
Mohnish <mohnish.a007@gmail.com>
2025-02-12 02:25:53 +0000

    usecase of void pointer and example

--- basic_refresher/pointers/pbasics.cpp
+++ basic_refresher/pointers/pbasics.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 void vptr(void* data, char datatype);
 int main() {
-   // void pointer use case
+   // void pointer use case. malloc function generally returns a void pointer in C. In C++ it is often the new function for object creation
    int i1=25;
    float f1 = 10.2;
    char c1 = 'a';
