ade4d094d68ea00fb9383046ea1e3b7d03ed2749
Mohnish <mohnish.a007@gmail.com>
2025-02-13 22:03:10 +0000

    travellinf array through pointer

--- basic_refresher/pointers/arrptr.cpp
+++ basic_refresher/pointers/arrptr.cpp
@@ -3,9 +3,11 @@ using namespace std;
 int main() {
 cout << "Array pointers" << endl;
 int arr[5];
+int* arrptr;
+arrptr = &arr[0];
 for (int i = 0; i < 5; i++){
     arr[i] = i;
-    cout << "Address of arr[" << i << "] = " << &arr[i] << ", Value of arr[" << i << "] = " << arr[i] << endl;
+    cout << "Address of arr[" << i << "] = " << &arr[i] << ", Value of arr[" << i << "] = " << *(arrptr + i) << endl;
 }
 // cout << "Array pointers" << endl;
 return 0;
