05eb7dc0bf80a26f18c72d0b876c581da7837b66
Mohnish <mohnish.a007@gmail.com>
2025-02-13 21:57:35 +0000

    pointer arthematic with array of integers

---
+++ basic_refresher/pointers/arrptr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout << "Array pointers" << endl;
+int arr[5];
+for (int i = 0; i < 5; i++){
+    arr[i] = i;
+    cout << "Address of arr[" << i << "] = " << &arr[i] << ", Value of arr[" << i << "] = " << arr[i] << endl;
+}
+// cout << "Array pointers" << endl;
+return 0;
+}
