c9da961c80e221ffae2475c693c70c6c2712d166
Mohnish <mohnish.a007@gmail.com>
2025-02-15 21:50:32 +0000

    function pointer and its calling and uses

--- basic_refresher/pointers/funcptr.cpp
+++ basic_refresher/pointers/funcptr.cpp
@@ -7,8 +7,12 @@ cout << "Function pointer and its applications" << endl;
 cout<< "Address of function foo is " << foo << endl;
 cout<< "Address of function foo is " << &foo << endl;
 //cout<< "Output after the function foo is called is " << foo() << endl;
+void (*ptr)(); // Declaration of fuction pointer ptr which return void type and accepts no arguments. 
+ptr = &foo;   // Storing the function address
+ptr();  // Calling the function through function pointer 
+cout << "Address of function foo using pointer ptr is " << ptr<<endl; // checking address of function foo
 return 0;
 }
 void foo(){
-    cout << "This is function foo" << endl;
+    cout << "This is function foo called from function pointer" << endl;
 }
