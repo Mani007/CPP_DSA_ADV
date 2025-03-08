f47fded67fd2ff5b160c7d35cb0a41854b6992c8
Mohnish <mohnish.a007@gmail.com>
2025-03-08 22:00:09 +0000

    basic inline function is working good!

--- basic_refresher/function/inlinefun.cpp
+++ basic_refresher/function/inlinefun.cpp
@@ -1,23 +1,17 @@
 #include <iostream>
 using namespace std;
 // Fuction accepting references as input
-int & swap(int &a,int &b);
+
+inline int product(int a, int b){
+    return a * b;  // Inline function call
+}
 int main() {
     int a=4,b=5;
 //int *p=&a, *q=&b;
     cout << "Inline functions" << endl;
-    int reff = 10;
-    int &reffvar = reff; // reffvar is a reference variable storing the address of reff variable
-    cout << "Call by value and call by reff " << endl;  // 2,3 are actual function parameters
-    cout << "Current value of a is "<< a << " and value of b is "<< b << endl;
-    swap(a, b) = 225; // this will swap a,b using reference variables
-    cout << "NEW value of a is "<< a << " and value of b is "<< b << endl;
+    cout << "Inline function product is called and result of 4x5 is "<< product(a,b) << endl;
+    
     return 0;
 }
-int & swap(int &a,int &b){   // returning a reference variable
-    int temp =a;
-    a = b;
-    b = temp;
-    return a;
-}
+
 
