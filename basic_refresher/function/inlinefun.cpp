2b2db2cb817d37e1318752e272a847875f54ee20
Mohnish <mohnish.a007@gmail.com>
2025-03-08 18:41:23 +0000

    swap function call by ref,  Working correctly

--- basic_refresher/function/inlinefun.cpp
+++ basic_refresher/function/inlinefun.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
-void swap(int,int);
+void swap(int*,int*);
 int main() {
 int a=4,b=5;
-int *p=&a, *q=&b;
+//int *p=&a, *q=&b;
 cout << "Inline functions" << endl;
 cout << "Call by value and call by reff " << endl;  // 2,3 are actual function parameters
 cout << "Current value of a is "<< a << " and value of b is "<< b << endl;
-swap(p, q);
+swap(&a, &b);
 cout << "NEW value of a is "<< a << " and value of b is "<< b << endl;
 
 return 0;
 }
-void swap(int *a,int *b){
+// Fuction accepting references as input
+void swap(int* a,int* b){
     int temp =*a;
     *a = *b;
     *b = temp;
