8ebb4b78ccf649c65e581fc5594c38bc8745677f
Mohnish <mohnish.a007@gmail.com>
2025-03-08 17:55:32 +0000

    void function prototype

--- basic_refresher/function/inlinefun.cpp
+++ basic_refresher/function/inlinefun.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+void greet(void); // function prototype
 int mul(int x, int y){  // x, y are formal parameters
     return x * y; 
 }
@@ -7,5 +8,10 @@ int main() {
 cout << "Inline functions" << endl;
 cout << "Inline functions call is " << mul(2,3) << endl;  // 2,3 are actual function parameters
 cout << "Inline functions" << endl;
+greet();
 return 0;
+}
+
+void greet(){
+    cout << "Hello, World!" << endl;
 }
