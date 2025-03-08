6fc6227b75426c0e2024da73d18fe322ed69e5d9
Mohnish <mohnish.a007@gmail.com>
2025-03-08 18:47:11 +0000

    swap the variable using ref variables NOT the pointers,  Working correctly

--- basic_refresher/function/inlinefun.cpp
+++ basic_refresher/function/inlinefun.cpp
@@ -5,17 +5,19 @@ int main() {
 int a=4,b=5;
 //int *p=&a, *q=&b;
 cout << "Inline functions" << endl;
+int reff = 10;
+int &reffvar = reff; // reffvar is a reference variable storing the address of reff variable
 cout << "Call by value and call by reff " << endl;  // 2,3 are actual function parameters
 cout << "Current value of a is "<< a << " and value of b is "<< b << endl;
-swap(&a, &b);
+swap(a, b);
 cout << "NEW value of a is "<< a << " and value of b is "<< b << endl;
 
 return 0;
 }
 // Fuction accepting references as input
-void swap(int* a,int* b){
-    int temp =*a;
-    *a = *b;
-    *b = temp;
+void swap(int &a,int &b){
+    int temp =a;
+    a = b;
+    b = temp;
 }
 
