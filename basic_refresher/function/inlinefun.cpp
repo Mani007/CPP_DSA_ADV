868992f01304d5f449956b5783016ba8709545f4
Mohnish <mohnish.a007@gmail.com>
2025-03-08 22:08:32 +0000

    Fucntion with default argument

--- basic_refresher/function/inlinefun.cpp
+++ basic_refresher/function/inlinefun.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
-// Fuction accepting references as input
-
-inline int product(int a, int b){
+// Make sure to choose when to make function inline
+// inline function is should not be used in recursions, static variables, in loops, in switch statements
+int product(int a, int b=0) { // Function with default parameters, default parameters are always at the end
     return a * b;  // Inline function call
 }
 int main() {
     int a=4,b=5;
 //int *p=&a, *q=&b;
-    cout << "Inline functions" << endl;
-    cout << "Inline function product is called and result of 4x5 is "<< product(a,b) << endl;
+    cout << "Normal functions" << endl;
+    cout << "Normal function product is called and result of 4x5 is "<< product(a,b) << endl;
+    cout << "Normal function product is called and result of 4x0 by default is "<< product(a) << endl;
     
     return 0;
 }
