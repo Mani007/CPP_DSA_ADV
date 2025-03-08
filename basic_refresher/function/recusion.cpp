e5184d17f882af64a0edd6cedf19079bd9cc72e9
Mohnish <mohnish.a007@gmail.com>
2025-03-08 23:20:54 +0000

    Basic fabonaci calculation program using recursion of function

--- basic_refresher/function/recusion.cpp
+++ basic_refresher/function/recusion.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
-int factorial(int num){
-    if(num == 1)
+int factorial(int num){ // Recursive fuction for calculating the factorials
+    if(num <= 1)
         return 1;
     else
         return(num * factorial(num - 1));
 }
+int fabonachi(int num){ // Recursive fuction for calculating the fabonachi series
+    if(num <= 1){
+        return 1;
+    } else 
+    return(fabonachi(num - 1) + fabonachi(num - 2));
+    }
 int main() {
     int a;
 cout << "Recursion in C++" << endl;
 cout<< "Enter the number between 1 to 10" << endl;
 cin>> a;
 cout <<"The factorial of " << a << " is "<< factorial(a)<< endl;
+cout <<"The fabonachi of " << a << " is "<< fabonachi(a)<< endl;
 return 0;
 }
