449a784027c8edc6a729181cb2b6a25044fe331b
Mohnish <mohnish.a007@gmail.com>
2025-03-08 21:48:56 +0000

    returning reference variable from function is not working

--- basic_refresher/function/inlinefun.cpp
+++ basic_refresher/function/inlinefun.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 using namespace std;
-void swap(int*,int*);
+int & swap(int*,int*);
 int main() {
-int a=4,b=5;
+    int a=4,b=5;
 //int *p=&a, *q=&b;
-cout << "Inline functions" << endl;
-int reff = 10;
-int &reffvar = reff; // reffvar is a reference variable storing the address of reff variable
-cout << "Call by value and call by reff " << endl;  // 2,3 are actual function parameters
-cout << "Current value of a is "<< a << " and value of b is "<< b << endl;
-swap(a, b);
-cout << "NEW value of a is "<< a << " and value of b is "<< b << endl;
-
-return 0;
+    cout << "Inline functions" << endl;
+    int reff = 10;
+    int &reffvar = reff; // reffvar is a reference variable storing the address of reff variable
+    cout << "Call by value and call by reff " << endl;  // 2,3 are actual function parameters
+    cout << "Current value of a is "<< a << " and value of b is "<< b << endl;
+    swap(a, b) = 225; // this will swap a,b using reference variables
+    cout << "NEW value of a is "<< a << " and value of b is "<< b << endl;
+    return 0;
 }
 // Fuction accepting references as input
-void swap(int &a,int &b){
+int & swap(int &a,int &b){   // returning a reference variable
     int temp =a;
     a = b;
     b = temp;
+    return a;
 }
 
