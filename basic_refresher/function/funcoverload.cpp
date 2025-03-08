3e6e72a41437e4b8b372ace3135718616443f39a
Mohnish <mohnish.a007@gmail.com>
2025-03-08 23:27:46 +0000

    Multiple function WITH function overloading

--- basic_refresher/function/funcoverload.cpp
+++ basic_refresher/function/funcoverload.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 using namespace std;
-int add2(int a, int b){
+int add(int a, int b){
+    //cout <<  " 2 args " << endl;
     return a + b;
 }
-int add3(int a, int b, int c){
+int add(int a, int b, int c){
+    //cout <<  " 3 args " << endl;
     return a + b + c;
 }
 int main() {
     int a=4,b=5,c=6;
 cout << "Function overloading" << endl;
-cout << "The sum of two numbers 4 and 5 is " << add2(a,b) << endl;
-cout << "The sum of three numbers 4,5 and 6 is " << add3(a,b,c) << endl;
+cout << "The sum of two numbers 4 and 5 is " << add(a,b) << endl;
+cout << "The sum of three numbers 4,5 and 6 is " << add(a,b,c) << endl;
 return 0;
 }
