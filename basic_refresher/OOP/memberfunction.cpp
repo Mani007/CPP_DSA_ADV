d5b487af7aa5b3bfb00487bfe30acb78059a131b
Mohnish <mohnish.a007@gmail.com>
2025-03-11 00:57:37 +0000

    Basic OOP program for member functions ones complement not working properly

--- basic_refresher/OOP/memberfunction.cpp
+++ basic_refresher/OOP/memberfunction.cpp
@@ -7,6 +7,8 @@ class BinaryReader{
     public:
         void read_binary();
         void chk_binary();
+        void onescomplement();
+        void display();
 };
 void BinaryReader::read_binary(){
             cout<<"Enter a binary string: "<<endl;
@@ -21,11 +23,35 @@ void BinaryReader::chk_binary(){
     }
     cout<<"Valid binary string."<<endl;
 }
+
+void BinaryReader::onescomplement(){
+    for(int i=0; i<s.length(); i++){
+        if(s[i]= '0' )
+        {
+            s[i] = '1';
+        } else 
+        {
+            s[i] = '0';
+        }
+        
+    }
+    
+}
+
+void BinaryReader::display(){
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout << s[i];
+    }
+    
+}
 int main() {
     cout << "Nesting of member function" << endl;
     //string s;
     BinaryReader br;
     br.read_binary();
     br.chk_binary();
+    br.onescomplement();
+    br.display();
     return 0;
 }
