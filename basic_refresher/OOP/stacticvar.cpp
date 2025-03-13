373865598a4e5b961182f35a18471f382c2f43ee
Mohnish <mohnish.a007@gmail.com>
2025-03-13 23:07:16 +0000

    Basic class withOUT the static variables in action

--- basic_refresher/OOP/stacticvar.cpp
+++ basic_refresher/OOP/stacticvar.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 class Employee {
     int id;
-    static int count; // Static member of the class
+    int count =0; // Without Static member of the class
     public:
         void setID(void) {
             cout<<"Enter the id of the employee "<< endl;
@@ -14,7 +14,7 @@ class Employee {
             cout<<"Total number of employees created is "<< count << endl;
         }
 };
-int Employee:: count; // Static variable of the class declared outside. Default value is zero. 
+//int Employee:: count; // Static variable of the class declared outside. Default value is zero. 
 int main() {
     cout << "Static variable in OOP" << endl;
     Employee emp1, emp2;
