a168cf9cbad30860e92c1f0102e1bf05764b2d43
Mohnish <mohnish.a007@gmail.com>
2025-03-14 00:10:26 +0000

    Basic class with the static Functions in class property

--- basic_refresher/OOP/stacticvar.cpp
+++ basic_refresher/OOP/stacticvar.cpp
@@ -14,15 +14,22 @@ class Employee {
             cout<<"The id of the employee is "<< id << endl;
             cout<<"Total number of employees created is "<< count << endl;
         }
+        static void getCount(void){
+            cout<<"Total number of employees created is "<< count << endl;
+        }
 };
-int Employee:: count; // Static variable of the class declared outside. Default value is zero.
+int Employee:: count =1000; // Static variable of the class declared outside. Default value is zero.
  
 int main() {
     cout << "Static variable in OOP" << endl;
+    // emp1 and emp2 shares the same static variables. 
     Employee emp1, emp2;
     emp1.setID();
     emp1.displayID();
+    Employee::getCount(); // Accessing static member function directly.
+    // static function is run directly from the class
     emp2.setID();
     emp2.displayID();
+    Employee::getCount(); // Accessing static member function directly.
 return 0;
 }
