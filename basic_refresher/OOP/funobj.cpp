870e25b9e3013835810728c0c40cada6491c6198
Mohnish <mohnish.a007@gmail.com>
2025-03-14 01:44:07 +0000

    Object as a fumction argument

---
+++ basic_refresher/OOP/funobj.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Complex {
+    int a,b;
+    public:
+        void setData(int x,int y){
+            a = x;
+            b = y;
+        }
+        void addComplex(Complex c1,Complex c2){
+            a = c1.a + c2.a;
+            b = c1.b + c2.b;
+        }
+        void displayComplex(void){
+            cout <<"The Complex number is "<< a << " + " << b << "i" << endl;
+        }
+        void displayComplexSum(void){
+            cout <<"The Complex number SUM is "<< a << " + " << b << "i" << endl;
+        }
+};
+int main() {
+cout << "Object as function argument" << endl;
+    Complex c1,c2,c3;
+    c1.setData(3,4);
+    c1.displayComplex();
+    c2.setData(1,2);
+    c2.displayComplex();
+    c3.addComplex(c1,c2);
+    c3.displayComplexSum();
+return 0;
+}
