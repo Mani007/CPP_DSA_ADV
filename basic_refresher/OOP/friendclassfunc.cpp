debeb9b84cc938ae3cca33883ec9412d7af56369
Mohnish <mohnish.a007@gmail.com>
2025-03-15 21:17:10 +0000

    friend add object does not run again due to private variable data

--- basic_refresher/OOP/friendclassfunc.cpp
+++ basic_refresher/OOP/friendclassfunc.cpp
@@ -4,14 +4,16 @@ class X{
     int data;
     public:
     void setValue(int value) { data = value; }
+    friend void addObj(X,Y);
 };
 class Y{
     int data;
     public:
     void setValue(int value) { data = value; }
+    friend void addObj(X,Y);
 };
 
-int addObj(X o1,Y o2){
+void addObj(X o1,Y o2){ // we need to declare it as a friend function to make it work.
     cout<< "The sum of data in object X and Y are "<< o1.data+o2.data<<endl;  // We cannot use it like this as data are private members of objects
 }
 int main() {
