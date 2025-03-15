ce15edcc65580311982a9a3b0cf9eb5ec1e6d085
Mohnish <mohnish.a007@gmail.com>
2025-03-15 23:20:55 +0000

    basics of constructor functions - with  parameter and implicit and explicit calls

--- basic_refresher/OOP/cunstructors.cpp
+++ basic_refresher/OOP/cunstructors.cpp
@@ -4,22 +4,26 @@ class Complex{
     int real,imag;
     public:
     //Constructor is a special member function with same name as class and it is called whenever the object is created. It is used to initialize the object.
-    Complex(void);
+    // Constructor should always be in public class and they invoke automatically. 
+    // Constructor does not have return types.
+    // We cannot refer to the address of constructor
+    Complex(int x, int y);
     void printData(void){
         cout << "Complex number is " << real << "+" << imag <<"i" << endl;
     }
     //Destructor is a special member function with a special name " ~classname" and it is called when an object goes out of scope. It is used to free up any dynamically allocated memory.
 };
 
-Complex::Complex(void){
-    real = 10;
-    imag = 20;
+Complex::Complex(int x, int y){ // Constructor with parameter is called parameterized constructor
+    real = x;
+    imag = y;
 }
 int main() {
     cout << "Constructors in CPP" << endl;
 
 //Creating object of Complex class
-    Complex c1,c2,c3;
+    Complex c1(2,3),c2(3,4); // Implicit call
+    Complex c3 = Complex(4,5); // Explicit call
     c1.printData(); // Output: Real part = 3, Imaginary part = 4
     c2.printData(); 
     c3.printData(); 
