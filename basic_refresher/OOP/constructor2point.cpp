afe356c3e677ddf62b0e2a76bb49f3ec14e20de1
Mohnish <mohnish.a007@gmail.com>
2025-03-15 23:35:46 +0000

    basics of constructor -points in 2D with constructor overloading

--- basic_refresher/OOP/constructor2point.cpp
+++ basic_refresher/OOP/constructor2point.cpp
@@ -7,10 +7,16 @@ class Point{
     int x,y,z;
     public:
     friend int Distance3D(Point p1, Point p2);
+    friend int Distance2D(Point p1, Point p2);
     Point(int a, int b, int c) {
         x = a;
         y = b;
         z = c;
+    }
+    Point(int a, int b){ // constructor overloading for point in 2D
+        x = a;
+        y = b;
+        z = 0;
     }
     void printPoint(void){
         cout << "Point in 3D are (" << x << ", " << y << ", " << z << ")" << endl;
@@ -22,14 +28,19 @@ int Distance3D(Point p1, Point p2){
     int dz = p2.z - p1.z;
     return sqrt(dx*dx + dy*dy + dz*dz);
 }
+int Distance2D(Point p1, Point p2){
+    int dx = p2.x - p1.x;
+    int dy = p2.y - p1.y;
+    return sqrt(dx*dx + dy*dy);
+}
 
 int main() {
 cout << "Constructor example - 3D points" << endl;
-    Point p1(1, 2, 3);
+    Point p1(1, 2);
     p1.printPoint();
-    Point p2(4, 5, 6);
+    Point p2(4, 5);
     p2.printPoint();
-    cout << "Distance between p1 and p2 is " << Distance3D(p1, p2) << endl;
+    cout << "Distance between p1 and p2 is " << Distance2D(p1, p2) << endl;
  
 
 return 0;
