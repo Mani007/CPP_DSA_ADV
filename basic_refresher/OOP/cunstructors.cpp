d8eb099eed6869c5e97cd0a1227adbaae6370970
Mohnish <mohnish.a007@gmail.com>
2025-03-15 22:44:21 +0000

    basics of constructor functions - without constructor and garbage value

--- basic_refresher/OOP/cunstructors.cpp
+++ basic_refresher/OOP/cunstructors.cpp
@@ -4,22 +4,22 @@ class Complex{
     int real,imag;
     public:
     //Constructor is a special member function with same name as class and it is called whenever the object is created. It is used to initialize the object.
-    Complex(int x, int y);
+    //Complex(int x, int y);
     void printData(void){
         cout << "Real part = " << real << ", Imaginary part = " << imag << endl;
     }
     //Destructor is a special member function with a special name " ~classname" and it is called when an object goes out of scope. It is used to free up any dynamically allocated memory.
 };
 
-Complex::Complex(int x, int y){
-    real = x;
-    imag = y;
-}
+// Complex::Complex(int x, int y){
+//     real = x;
+//     imag = y;
+// }
 int main() {
     cout << "Constructors in CPP" << endl;
 
 //Creating object of Complex class
-    Complex c1(3, 4);
+    Complex c1;
     c1.printData(); // Output: Real part = 3, Imaginary part = 4
 return 0;
 }
