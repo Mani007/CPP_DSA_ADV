95c7ed1bad85a7b23035579e569182bd509479d2
Mohnish <mohnish.a007@gmail.com>
2025-03-16 21:12:22 +0000

    Default Copy constructor by compiler in action

--- basic_refresher/OOP/copyconstructor.cpp
+++ basic_refresher/OOP/copyconstructor.cpp
@@ -11,10 +11,10 @@ class Number{
         number = num;
     }
     //A copy constructor taking reference of the original Number object
-    Number(const Number &obj){ // Copy constructor
-        cout<<"Copy constructor called"<<endl;
-        number = obj.number;
-    }
+    // Number(const Number &obj){ // Copy constructor
+    //     cout<<"Copy constructor called"<<endl;
+    //     number = obj.number;
+    // }
 
     void display(void){
         cout << "Number: " << number << endl;
@@ -27,8 +27,8 @@ Number n1(10); // Constructor with argument
 
 Number n2, n3; // General object using generic constructor
 n1.display();
-Number n4(n1); // creating n4 object using copy constructor
+Number n4(n1); // creating n4 object using Default copy constructor by Compiler
 n4.display();
-// Copy constructor should exactly copy n1 
+
 return 0;
 }
