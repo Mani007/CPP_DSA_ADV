237825bef9f6ff5e642d7292bbbffbfc88aa8499
Mohnish <mohnish.a007@gmail.com>
2025-03-16 20:31:31 +0000

    Constructor for interest calculator for bank

---
+++ basic_refresher/OOP/Constructor4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class Bankdeposit{
+    int principle,year;
+    float rate,amount;
+    public:
+    Bankdeposit(int p, int y, float r=0.04){
+        principle = p;
+        year = y;
+        rate = r;
+        amount = principle;
+        for (int i = 0; i <year; i++){
+
+            amount = amount *(1+rate);
+        }
+    }
+    void show(void){
+        cout << "Amount after " << year << " years: " << amount << endl;
+    }
+};
+int main() {
+    cout << "Interest calculator" << endl;
+    int p,t;
+    float r;
+
+    cout << "Enter the principal amount: ";
+    cin >> p;
+
+    cout << "Enter the rate of interest (in 0.0X): ";
+    cin >> r;
+
+    cout << "Enter the time period in years: ";
+    cin >> t;
+
+    Bankdeposit bd(p,t,r);
+
+    bd.show();
+return 0;
+}
