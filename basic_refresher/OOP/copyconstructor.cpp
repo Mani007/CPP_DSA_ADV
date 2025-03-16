a2f97250d85b4aa28abdb5e735cf946f58411a1d
Mohnish <mohnish.a007@gmail.com>
2025-03-16 21:15:28 +0000

    Copy constructor invoked with assignment

--- basic_refresher/OOP/copyconstructor.cpp
+++ basic_refresher/OOP/copyconstructor.cpp
@@ -11,10 +11,11 @@ class Number{
         number = num;
     }
     //A copy constructor taking reference of the original Number object
-    // Number(const Number &obj){ // Copy constructor
-    //     cout<<"Copy constructor called"<<endl;
-    //     number = obj.number;
-    // }
+    Number(const Number &obj){ // Copy constructor
+        cout<<"Copy constructor called"<<endl;
+        number = obj.number;
+    }
+    // When there is no copy constructor found, compiler will run its default copy constructor 
 
     void display(void){
         cout << "Number: " << number << endl;
@@ -29,6 +30,8 @@ Number n2, n3; // General object using generic constructor
 n1.display();
 Number n4(n1); // creating n4 object using Default copy constructor by Compiler
 n4.display();
+n2 = n1;  // This assignment will also invoke our copy constructor
+n2.display();
 
 return 0;
 }
