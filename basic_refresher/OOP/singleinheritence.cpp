2eb1c447dbf5be01d2198f8e93babcf9c3431dae
Mohnish <mohnish.a007@gmail.com>
2025-03-18 00:06:33 +0000

    Single inheritance example setup

---
+++ basic_refresher/OOP/singleinheritence.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class Base{
+    int data1;// by default private and is not inheritable
+    public:
+        int data2;
+        void setData();
+        int getData1();
+        int getData2();
+
+};
+void Base::setData(void){
+    data1 = 10;
+    data2 = 20;
+}
+
+int Base::getData1(){
+    return data1;
+}
+
+int Base::getData2(){
+    return data2;
+}
+class Derived: public Base{ // class is derived public from Base class
+    int data3;
+    public:
+        void processData(void);
+        void display(void);
+
+};
+void Derived::processData(void){
+    data3 = data2 * getData1();
+}
+void Derived::display(void){
+    cout<<"The value of data1 in Base class is "<<getData1()<<endl;
+    cout<<"The value of data2 in Base class is "<<data2<<endl;
+    cout<<"The value of data3 in Derived class is "<<data3<<endl;
+}
+int main() {
+    cout << "Single inheritance" << endl;
+    Derived der;
+    der.setData(); // Yes we call call all the public method from the derived class to base class. This method will set value of data1 and data2
+    der.processData(); // This function is called from derived class only
+    der.display(); // This function is called from derived class only
+return 0;
+}
