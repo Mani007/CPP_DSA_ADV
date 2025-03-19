a3c5275ba25de702d6fe4b99e285abe4189d43aa
Mohnish <mohnish.a007@gmail.com>
2025-03-19 02:03:27 +0000

    Setup for ambiguity resolution in multiple inheritance- ambiguity Resolved with function overriding

--- basic_refresher/OOP/ambiguityinheritance.cpp
+++ basic_refresher/OOP/ambiguityinheritance.cpp
@@ -22,6 +22,12 @@ class Derived : public Base1, public Base2{
         Base1::greet(); // This will call the greet() from Base1
     }
 };
+class Derived1: public Base1{ // single inheritance
+    public:
+    void greet(void){ // Ambiguity overriding in derived class
+        cout << "Hello from Derived1" << endl;
+    }
+};
 int main() {
     cout << "Ambiguity in C++ inheritance" << endl;
     Base1 base1obj;
@@ -30,5 +36,7 @@ int main() {
     base2obj.greet();
     Derived derivedObj;
     derivedObj.greet(); // Ambiguity resolved by MRO (Method Resolution Order)
+    Derived1 derived1Obj;
+    derived1Obj.greet(); // Ambiguity overridden in derived class
 return 0;
 }
