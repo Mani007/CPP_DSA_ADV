8f220a052a0e0f85b34f4ab290c191feb9479f95
Mohnish <mohnish.a007@gmail.com>
2025-03-20 19:50:22 +0000

    class pointer to array of classes - user input

--- basic_refresher/OOP/classpointerarray.cpp
+++ basic_refresher/OOP/classpointerarray.cpp
@@ -5,25 +5,32 @@ class Shop{
     int price;
     public:
     void setData(int id1, int price1) { id = id1; price = price1; } // We need different name for local variables, other wise we need to use the "this" pointer
-    int getData(void) {
-        cout<<"id: "<<id<<"price: "<<price<<endl;
+    void getData(void) {
+        cout<<"id: "<<id<<" and price: "<<price<<endl;
     }
 };
 int main() {
 cout << "Class pointer to the array of object" << endl;
-    int size=2;
+    int size=3;
     Shop* shopArray = new Shop[size]; // dynamic memory allocation
     //Shop shopArray[size];
     // shopArray[0].setData(1, 500);
     // shopArray[1].setData(2, 700);
     // shopArray[0].getData();
     // shopArray[1].getData();
-    for(int i=0; i<size; i++) {
-        shopArray->setData(i+1, i*100);
-        shopArray->getData();
-        shopArray++; // increment the pointer to the next object in the array
+    // for(int i=0; i<size; i++) {
+    //     shopArray->setData(i+1, i*100);
+    //     shopArray->getData();
+    //     shopArray++; // increment the pointer to the next object in the array
+    // }
+    int p,q;
+    for(int i; i<size; i++) {
+    cout<<"Enter the id and price for shop: "<<i+1<<endl;
+    cin>>p>>q;
+        shopArray[i].setData(p, q);
+        shopArray[i].getData();
     }
-    delete[] shopArray; // de-allocate memory
+    // delete[] shopArray; // de-allocate memory
     
 return 0;
 }
