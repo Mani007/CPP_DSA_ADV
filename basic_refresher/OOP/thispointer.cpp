e96f545f9f73cbf1edd0d573af945931c74b1678
Mohnish <mohnish.a007@gmail.com>
2025-03-21 02:59:14 +0000

    class pointer and this pointer - another use of this pointer and method chaining as we return reference varable of the object

--- basic_refresher/OOP/thispointer.cpp
+++ basic_refresher/OOP/thispointer.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 class A{
-    int a;
+    int a; // This is a class variable not a local variable
     public:
-    void setData(int a){  // we cannot declare the variable name same as class variable. To make this work we need to use "this" pointer  
-        this->a=a; // "this" is a class self referential pointer
+    A& setData(int a){  // returning the reference variable of the object
+        this->a=a; // "this" is a class self referential pointer as priority is always given to local variables and hence use of this pointer will differentiate from the variable we are talking about
+        return *this;  // Another use of this pointer to return reference variable of current object 
     }
     void getData(void){
         cout<<"The value of a is "<<a<<endl;
@@ -14,7 +15,7 @@ class A{
 int main() {
     cout << "This pointer in C++" << endl;
     A x;
-    x.setData(5);
-    x.getData();
+    x.setData(5).getData();  // Because we have self referential pointer we can do method chaining for object
+    //x.getData();
 return 0;
 }
