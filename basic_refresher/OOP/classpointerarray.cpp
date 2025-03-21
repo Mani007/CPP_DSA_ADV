aadb91324acbf44e30cc7e9960defc0ba052cecc
Mohnish <mohnish.a007@gmail.com>
2025-03-21 02:45:06 +0000

    class pointer to array of classes - error fixed using two pointer approch as we do in doubly linked list

--- basic_refresher/OOP/classpointerarray.cpp
+++ basic_refresher/OOP/classpointerarray.cpp
@@ -12,26 +12,19 @@ class Shop{
 int main() {
 cout << "Class pointer to the array of object" << endl;
     int size=3;
-    Shop* shopArray = new Shop[size]; // dynamic memory allocation
-    Shop* tempArray = shopArray; // storing initial value of the pointer Very inportant
-    //Shop shopArray[size];
-    // shopArray[0].setData(1, 500);
-    // shopArray[1].setData(2, 700);
-    // shopArray[0].getData();
-    // shopArray[1].getData();
-    // for(int i=0; i<size; i++) {
-    //     shopArray->setData(i+1, i*100);
-    //     shopArray->getData();
-    //     shopArray++; // increment the pointer to the next object in the array
-    // }
-    int p,q;
-    for(int i; i<size; i++) {
-    cout<<"Enter the id and price for shop: "<<i+1<<endl;
-    cin>>p>>q;
+    Shop *shopArray = new Shop[size]; // dynamic memory allocation
+    Shop *tempArray = shopArray; // storing initial value of the pointer Very inportant
+   
+    cout<<"the value of pointer 1 is "<< shopArray<<" and pointer 2 is "<<tempArray<<endl;
+    int p,q,i,j;
+    for(i=0; i<size; i++) {
+        cout<<"Enter the id and price for shop: "<<i+1<<endl;
+        cin>>p>>q;
         shopArray->setData(p, q);
         shopArray++;
     }
-    for(int i;i<size;i++){
+    for(j=0;j<size;j++){
+        cout<<"Display for shop: "<<j+1<<endl;
         tempArray->getData();
         tempArray++; // increment the pointer to the next object in the array
     }
