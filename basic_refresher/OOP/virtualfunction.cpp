68347f027ff4e74048b9de2f59ea3c0c37154c5b
Mohnish <mohnish.a007@gmail.com>
2025-03-22 03:48:06 +0000

    Polymorphism in action -Virtual class setup

---
+++ basic_refresher/OOP/virtualfunction.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class BaseClass{
+    public:
+    int var_base;
+    void display(){
+        cout << "Base class display "<<" var_base value is "<<var_base << endl;
+    }
+};
+
+class DerivedClass: public BaseClass{
+    public:
+    int var_derived;
+    void display(){
+        cout << "FROM THE DERIVED CLASS Base class display"<<"var_base value is "<<var_base << endl;
+        cout << "Derived class display" << ", var_derived value is "<<var_derived << endl;
+    }
+};
+int main() {
+    cout << "Virtual function and run time polymorphism" << endl;
+    BaseClass *baseptr;
+    BaseClass obj_base;
+    DerivedClass obj_derived;
+    baseptr = &obj_derived; // base class pointer referring to derived class object
+    baseptr->var_base=34;
+    baseptr->display(); // this will run display function of base class although it is referring to derived class
+return 0;
+}
