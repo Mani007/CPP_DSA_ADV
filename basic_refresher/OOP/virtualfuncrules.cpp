4f243137323bd4eb79c98fe27731aef085c70e02
Mohnish <mohnish.a007@gmail.com>
2025-03-22 20:52:43 +0000

    Polymorphism in action - basic runtime polymorphism setup

---
+++ basic_refresher/OOP/virtualfuncrules.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+class Rating{
+    protected:
+    char title[30];
+    float rating;
+    public:
+    Rating(char* t, float r){
+        strcpy(title, t);
+        rating = r;
+    }
+    virtual void display(){
+        cout << "Title: " << title << ", Rating: " << rating << endl;
+    }
+};
+
+class MovieRating: public Rating{
+    int length;
+    public:
+    MovieRating(char* t, float r,int l):Rating(t, r){
+        // Constructor for MovieRating class
+        length = l;    
+    }
+    void display(){
+        cout << "Title: " << title << ", Rating: " << rating << ", Genre: Movie"<<" and length is "<< length << endl;
+    }
+};
+
+class SeriesRating: public Rating{
+    int episode;
+    public:
+    SeriesRating(char* t, float r,int e):Rating(t, r){
+        // Constructor for SeriesRating class 
+        episode = e;   
+    }
+    void display(){
+        cout << "Title: " << title << ", Rating: " << rating << ", Genre: Series"<<" total episodes "<< episode << endl;
+    }
+};
+int main() {
+    cout << "Virtual function creation rules" << endl;
+    // A virtual function must be declared in the base class and overridden in the derived class.
+    // A derived class must have at least one virtual function to override.
+    // A class can have multiple virtual functions but only one non-virtual function.
+    // Virtual functions can be overridden using the "override" and "final" keywords.
+    // A class cannot have a virtual constructor or destructor.
+    // Virtual functions can be called using a pointer or a reference to the base class.
+    Rating* ptr;
+    MovieRating movie("Inception", 8.9, 150);
+    SeriesRating series("Game of Thrones", 9.3, 85);
+    ptr = &movie;
+    ptr->display(); // Calls the display() function of MovieRating class
+    ptr = &series;
+    ptr->display(); // Calls the display() function of SeriesRating class
+    
+return 0;
+}
--- basic_refresher/OOP/virtualfunction.cpp
+++ basic_refresher/OOP/virtualfunction.cpp
@@ -3,6 +3,7 @@ using namespace std;
 class BaseClass{
     public:
     int var_base;
+    // run time polymorphism is achieved
     virtual void display(){ // making function virtual such that if derived class reference pointer (pointing to base class) calls for display then run the display function in derived class only. Virtual function donates their priority.
         cout << "Base class display "<<" var_base value is "<<var_base << endl;
     }
