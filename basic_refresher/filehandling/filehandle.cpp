715e57796d355eff3dcfd0680f313703045c701f
Mohnish <mohnish.a007@gmail.com>
2025-03-22 23:06:38 +0000

    basic file handling setup

--- basic_refresher/filehandling/filehandle.cpp
+++ basic_refresher/filehandling/filehandle.cpp
@@ -1,7 +1,21 @@
 // This required understanding of OOP
 #include <iostream>
+#include <fstream>
 using namespace std;
+/*
+Useful class in fstream for file handling in C++ are 
+1. fstreambase class
+2. ifstream (input file stream) ---derived from fstream base class
+3. ofstream (output file stream) ---derived from fstream base class
+4.fstream (file input/output stream)
+*/
+/*
+We can open a file using two methods
+1. using the constructor
+2. using the member function open() 
+*/
 int main() {
-cout << "/* message */" << endl;
+    cout << "File handling in C++" << endl;
+    ofstream("sample.txt");
 return 0;
 }
