a2af52a7d00d170c343b54b0cbcfeeb3c365bfe4
Mohnish <mohnish.a007@gmail.com>
2025-03-31 14:14:17 +0100

    basic use of templates

--- CP/templates_basics/templates1.cpp
+++ CP/templates_basics/templates1.cpp
@@ -2,22 +2,23 @@
 using namespace std;
 /* Templates or Generics are generalized classes 
 */
+template <class T>  // T can be anything int,float, object etc. 
 class Vector1{
     public:
-    int *arr;
+    T *arr; // passing the template variable here
     int size;
     Vector1(int n){
         size = n;
-        arr = new int[size];
+        arr = new T[size];  // passing the template variable here
     }
-    int sum(Vector1 &v){
-        int sum = 0;
+    T sum(Vector1 &v){  // passing the template variable here
+        T sum = 0;
         for(int i = 0; i < size; i++)
             sum += arr[i] + v.arr[i];
         return sum;
     }
-    int dotproduct(Vector1 &v){
-        int sum = 0;
+    T dotproduct(Vector1 &v){ // passing the template variable here
+        T sum = 0;  // passing the template variable here as it returns T as output
         for(int i = 0; i < size; i++){
             sum += this->arr[i] * v.arr[i]; // this refer to current object and v refer to object we have passes to the function.this is also called as self referential pointer.
         }
@@ -27,13 +28,13 @@ class Vector1{
 int main() {
 cout << "Templates in CPP" << endl;
 
-    Vector1 v1(3);
+    Vector1 <int>v1(3);
     //v1->arr =12; // We cannot use this as we do not have pointer for the object as we have not allocated memory using new keyword syntax. ERROR: has non-pointer type vector1
     v1.arr[0] = 1;
     v1.arr[1] = 2;
     v1.arr[2] = 3;
 
-    Vector1 v2(3);
+    Vector1 <int>v2(3);
     v2.arr[0] = 4;
     v2.arr[1] = 5;
     v2.arr[2] = 6;
