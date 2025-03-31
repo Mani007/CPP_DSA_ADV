38f090d4e722eee5f9a9e7ed007e37511727913b
Mohnish <mohnish.a007@gmail.com>
2025-03-31 14:54:31 +0100

    function template in action-swapping function using template

--- CP/templates_basics/functiontemplates.cpp
+++ CP/templates_basics/functiontemplates.cpp
@@ -4,15 +4,29 @@ template <class T1,class T2>
 int funcAVG(T1 a ,T2 b){
     return (a+b)/2;
 }
+template <class T>
+void swapping(T & a,T &b){
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
 int main() {
 cout << "Function template" << endl;
 cout << "Another way to do polymorphism and function overloading" << endl;
 
-int result = funcAVG(5, 10);   // The result will be integer
+// int result = funcAVG(5, 10);   // The result will be integer
 
-cout << "Average of 5 and 10 is: " << result << endl;
+// cout << "Average of 5 and 10 is: " << result << endl;
 
-double result1 = funcAVG(5.5, 10.6); // This will  use of template to make answer double
-cout << "The average of 5.5 and 10.6 is "<<result1 << endl;
+// double result1 = funcAVG(5.5, 10.6); // This will  use of template to make answer double
+// cout << "The average of 5.5 and 10.6 is "<<result1 << endl;
+    int p=20,q=30;
+    swapping(p,q);
+    cout << "After swapping, p is "<< p << " and q is " << q << endl;
+    char x='a',y='b';
+    swapping(x,y);
+    cout << "After swapping, x is "<< x << " and y is " << y << endl;
+    
 return 0;
 }
