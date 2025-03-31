524b2a8bb939f7345459a6e9a8d69e762f68fd71
Mohnish <mohnish.a007@gmail.com>
2025-03-31 14:52:11 +0100

    function template in action"

--- CP/templates_basics/functiontemplates.cpp
+++ CP/templates_basics/functiontemplates.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
-int funcAVG(int a ,int b){
+template <class T1,class T2>
+int funcAVG(T1 a ,T2 b){
     return (a+b)/2;
 }
 int main() {
 cout << "Function template" << endl;
+cout << "Another way to do polymorphism and function overloading" << endl;
 
-int result = funcAVG(5, 10);
+int result = funcAVG(5, 10);   // The result will be integer
 
 cout << "Average of 5 and 10 is: " << result << endl;
 
-double result1 = funcAVG(5.5, 10.5); // This will require use of template
+double result1 = funcAVG(5.5, 10.6); // This will  use of template to make answer double
+cout << "The average of 5.5 and 10.6 is "<<result1 << endl;
 return 0;
 }
