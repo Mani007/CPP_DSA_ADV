8d8c5419e2450414bea34165282bdc859df8f683
Mohnish <mohnish.a007@gmail.com>
2025-03-31 21:40:20 +0100

    template: setup for member function and member class

---
+++ CP/templates_basics/templatemember.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+template <class T>
+class Base{
+    public:
+        T data;
+        Base(T d){
+            data = d;
+        }
+        void show(){
+            cout << "The value using show function is: " << data << endl;
+        }
+};
+int main() {
+cout << "Member function and overloading of template function" << endl;
+    Base<int> obj1(10);
+    obj1.show();
+    Base<float> obj2(10.5);
+    obj2.show();
+    Base<char> obj3('a');
+    obj3.show();
+    cout<<"The value using cout is: " << obj1.data<<endl;
+
+return 0;
+}
