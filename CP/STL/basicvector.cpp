f8c0e3b36874b0bb9ceee1a76858a5e77aa6f837
Mohnish <mohnish.a007@gmail.com>
2025-04-01 15:35:13 +0100

    Vector: basic program with user input with size and insert using iterator at second location

--- CP/STL/basicvector.cpp
+++ CP/STL/basicvector.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void display(vector<int> &v){
     for(int i=0; i<v.size();i++){
         cout << "Vector element at index "<<i <<" is "<< v[i] <<endl;
+        cout << "********************END HERE*******************"<<endl;
     }
 }
 int main() {
@@ -26,7 +27,7 @@ int main() {
     }
     display(vi); //
     vector<int> :: iterator iter = vi.begin(); // creating iterator and resolving scope. template<type> scope-resolved to create iterator iter assigned ref at begining of vector 
-    vi.insert(iter,33);  // iter is pointer to vi at the begining
+    vi.insert(iter+1,34);  // iter is pointer to vi at the second location
     display(vi); //
     
     
