4e8a05ce748dd92a6d3a66c10ec3504ce77174f4
Mohnish <mohnish.a007@gmail.com>
2025-04-01 19:19:00 +0100

    list: with int type working

--- CP/STL/basiclist.cpp
+++ CP/STL/basiclist.cpp
@@ -2,6 +2,16 @@
 #include <list>
 using namespace std;
 // Linked list are non contiguous block of memory 
+//template <class T>
+void displaylist(list<int> &lst){
+    cout<<"display started here"<<endl;
+    list<int> :: iterator it ;
+    for (it=lst.begin(); it != lst.end(); it++){
+        cout <<"The value of  " <<" is "<< *it << " "<<endl;
+    }
+    cout<<"display ended here"<<endl;
+    // lst.clear(); // To delete all elements in the list.
+}
 int main() {
     cout << "Basic list - linked list in C++" << endl;
     list<int> l1;
@@ -12,12 +22,14 @@ int main() {
     l2.push_back('a');
     l2.push_back('b');
     l2.push_back('c');
-    list<int>::iterator it1 = l1.begin();
-    list<char>::iterator it2 = l2.begin();
-    while(it1!=l1.end() && it2!=l2.end()) {
-        cout <<"Form l1: "<< *it1 << " and " <<"from l2: "<< *it2 << endl;
-        it1++;
-        it2++;
-    }
+    // list<int>::iterator it1 = l1.begin();
+    // list<char>::iterator it2 = l2.begin();
+    // while(it1!=l1.end() && it2!=l2.end()) {
+    //     cout <<"Form l1: "<< *it1 << " and " <<"from l2: "<< *it2 << endl;
+    //     it1++;
+    //     it2++;
+    // }
+    displaylist(l1);
+    //displaylist(l2);
 return 0;
 }
