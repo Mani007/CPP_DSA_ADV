4e8864313cf9bffec2e76f9b691fea80a496d061
Mohnish <mohnish.a007@gmail.com>
2025-04-01 15:36:14 +0100

    Vector: basic program with user input with size and insert using iterator at second location and multiple copies

--- CP/STL/basicvector.cpp
+++ CP/STL/basicvector.cpp
@@ -27,7 +27,7 @@ int main() {
     }
     display(vi); //
     vector<int> :: iterator iter = vi.begin(); // creating iterator and resolving scope. template<type> scope-resolved to create iterator iter assigned ref at begining of vector 
-    vi.insert(iter+1,34);  // iter is pointer to vi at the second location
+    vi.insert(iter+1,3,34);  // iter is pointer to vi at the second location and 3 copies
     display(vi); //
     
     
