e8b4c7f3bcdcc7709b688b890cfb0cf46f16cc51
Mohnish <mohnish.a007@gmail.com>
2025-04-02 22:37:42 +0100

    ADT array with create,set and show function with instructions

--- DSbasics/ArrayADT/arrayADT.c
+++ DSbasics/ArrayADT/arrayADT.c
@@ -4,12 +4,12 @@
 struct myArray{
     int total_size;
     int used_size;
-    int *ptr;
+    int *ptr;  // self referential pointer to the total size and can be accessed using index
 };
 void creatArray(struct myArray *arrayptr, int tsize, int usize){
     arrayptr->total_size = tsize;
     arrayptr->used_size = usize;
-    arrayptr->ptr = (int*) malloc(tsize * sizeof(int));
+    arrayptr->ptr = (int*) malloc(tsize * sizeof(int)); // total required size of the array is allocated and address of the first element is written as int pointer
     if(arrayptr->ptr == NULL){
         printf("Memory error!\n");
         exit(0);
@@ -30,14 +30,17 @@ void setarray(struct myArray *arrayptr){
     int i;
     printf("Enter %d integers: ", arrayptr->used_size);
     for(i=0; i<arrayptr->used_size; i++){
-        scanf("%d", &arrayptr->ptr[i]);
+        scanf("%d", &arrayptr->ptr[i]);  // scanf require address of the pointer
     }
     return;
 }
 int main() {
     struct myArray marks;
-    creatArray(&marks,5,2);
+    printf("Creating and initializing an array\n");
+    creatArray(&marks,5,4);
+    printf("Entering values for the array\n");
     setarray(&marks);
+    printf("Displaying the array\n");
     showarray(&marks);
 return 0;
 }
