444a48f4332b4d1c1e1120ceb11f9477b5f7d858
Mohnish <mohnish.a007@gmail.com>
2025-04-03 17:48:05 +0100

    traversal not outputting but working

--- DSbasics/linkedList/createLL.c
+++ DSbasics/linkedList/createLL.c
@@ -4,6 +4,15 @@ struct Node{
     int data;
     struct Node *next;
 };
+void lltraversal(struct Node *ptr){
+    int i=0;
+    while(ptr!=NULL){
+        printf("Data at %d node is  %d \n",i, ptr->data);
+        i++;
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
 int main() {
     printf("Lined List creationand traversal\n");
     printf("Creating all the pointers for all the list \n ");
@@ -24,6 +33,9 @@ int main() {
     // Making last point to the last node in the list and linking it to the NULL for termination
     last->data = 10;
     last->next = NULL; // last will always point to NULL in the list
+    lltraversal(first);
+    printf("All the elements of the linked list as been printed \n");
+    // Deleting the memory allocated for the nodes
 
 return 0;
 }
