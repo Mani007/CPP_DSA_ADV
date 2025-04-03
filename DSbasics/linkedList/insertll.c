c9376ffd7a2ca27a928bb4e2f5625f8ea5ef96fd
Mohnish <mohnish.a007@gmail.com>
2025-04-03 19:55:12 +0100

    inserting element using index in the middle of linked list - use diagram to understand better

--- DSbasics/linkedList/insertll.c
+++ DSbasics/linkedList/insertll.c
@@ -23,8 +23,23 @@ struct Node* insertfirst(struct Node *head,int data){
     struct Node *newnode = (struct Node*)malloc(sizeof(struct Node));
     newnode->data = data;
     newnode->next = head;
-    //head = newnode; // replacing the head node memory address to the newnode memory address
-    return newnode;
+    head = newnode; // replacing the head node memory address to the newnode memory address
+    return head;
+ }
+ struct Node* insertindex(struct Node *head,int data,int index){
+    struct Node *newnode = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *p = head; // link list traversal pointer
+    int i=0;
+    while(p!=NULL && i<index-1){ // traversal of linked list
+        p = p->next;
+        i++;
+    }
+    // use diagram to visualize and understand better. 
+    newnode->next = p->next; // new link is been establish here
+    newnode->data = data; // assigning the data to the new node
+    p->next = newnode; // new new connection is been completed
+    return head; // returning the head/first node to the main function
+
  }
 int main() {
     printf("Linked list insertion\n");
@@ -48,7 +63,8 @@ int main() {
     last->data = 10; // No segmentation error here now
     last->next = NULL; // last will always point to NULL in the list
     // Traversal of linkedlist nodes
-    first = insertfirst(first,55); // Inserting element in the begining of the list 
+    first = insertfirst(first,55); // Inserting element in the begining of the list
+    first = insertindex(first,88,4); // Inserting element in the middle of the list 
     lltraversal(first);
     printf("All the elements of the linked list as been printed \n");
     // Deleting the memory allocated for the nodes
