917ae0f62e427dc069f478db15b78c550c20aed1
Mohnish <mohnish.a007@gmail.com>
2025-04-03 16:44:43 +0100

    Binary search algorithm implemented

--- DSbasics/basicSearch/linandbinsearch.c
+++ DSbasics/basicSearch/linandbinsearch.c
@@ -6,6 +6,21 @@ int LinearSearch(int arr[],int size,int element){
     }
     return -1; // element not found in array
 }
+int BinarySearch(int arr[],int size,int element){
+    int mid,low,high;
+    low = 0;
+    high = size - 1;
+    while(low <= high){
+        mid = (low + high)/2;
+        if(arr[mid] == element)
+            return mid;
+        else if(arr[mid] < element)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1; // element not found in array
+}
 int main() {
     printf("Linear and Binary search algorithm \n");
     // Linear search algorithm using array traversal. O(n) time complexity. This algorithm work for sorted as well as unsorted arrays.
@@ -17,8 +32,19 @@ int main() {
         printf("%d is not present in array\n", element);
     }
     else{
-        printf("%d is present at index %d\n", element, result);
+        printf("In linear search %d is present at index %d\n", element, result);
     }
     // Binary search is algorithm like opening up a page in a book using page number. It will work only for sorted array. O(log(n)) time complexity
+    printf("Binary search starts here \n");
+    int arr1[] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    size = sizeof(arr1)/sizeof(arr1[0]);
+    element = 12;
+    result = BinarySearch(arr1, size, element);
+    if(result == -1){
+        printf("%d is not present in array\n", element);
+    }
+    else{
+        printf("In binary search %d is present at index %d\n", element, result);
+    }
 return 0;
 }
