695978224710d0d82bcc5b588e895efbc254daf1
Mohnish <mohnish.a007@gmail.com>
2025-04-03 15:49:42 +0100

    Array insertion and call by reference

--- DSbasics/ArrayADT/arrayinsert.c
+++ DSbasics/ArrayADT/arrayinsert.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
+#define CAPACITY 10
 void display(int arr[],int n){   // Array traversal function
+    // Traversal 
     for(int i=0; i<n; i++){
-        printf("Value at %d is %d \n",i, arr[i]);
+        printf("Value at %d is %d \t",i, arr[i]);
     }
     printf("\n");
     return;
 }
+int insertin(int arr[],int *ptrsize,int element,int index,int capacity){
+    if (*ptrsize>=capacity){
+        return -1;
+    } else {
+        // reverse loop
+        for (int i=*ptrsize-1;i>=index;i--){
+            arr[i+1]=arr[i];
+        }
+        arr[index]=element;
+        *ptrsize = *ptrsize+1; // do not use ptrsize++ as it will only update pointer and does not increase the size variable, instead use deref properly
+        printf("Item inserted successfully \n");
+        return 1;
+    }
+}
 int main() {
     printf("Array insertion operation \n");
-    int arr[10]={2,4,5,6,7,1};
+    int size=6,element=25,index=3;
+    int arr[CAPACITY]={2,4,5,6,7,1};
     // Array traversal function
-    display(arr,5);
+    display(arr,size);
+    insertin(arr,&size,element,index,CAPACITY);
+    display(arr,size);
 return 0;
 }
