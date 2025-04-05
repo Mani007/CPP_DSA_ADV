712c24b14241774b8cf6dbbc86cbe906d5a96506
Mohnish <mohnish.a007@gmail.com>
2025-04-05 13:30:17 +0100

    very basic stack implementation not working using pointer

--- DSbasics/stack/stackarray.c
+++ DSbasics/stack/stackarray.c
@@ -7,18 +7,29 @@ struct Stack{
 };
 int main() {
     printf("Stack using Array\n");
-    struct Stack s;
-    s.maxSize = 10;
-    s.top = -1;
-    s.arr = (int*)malloc(s.maxSize * sizeof(int));// allocate memory for given size of array
-    // push operation
-    s.arr[++s.top] = 10;
-    s.arr[++s.top] = 20;
-    s.arr[++s.top] = 30;
-    // pop operation
-    printf("Popped element is %d\n", s.arr[s.top--]);
-    printf("Popped element is %d\n", s.arr[s.top--]);
-    free(s.arr); // free memory allocated for array
+    // struct Stack s;
+    // s.maxSize = 10;
+    // s.top = -1;
+    // s.arr = (int*)malloc(s.maxSize * sizeof(int));// allocate memory for given size of array
+    //  push operation
+    // s.arr[++s.top] = 10;
+    // s.arr[++s.top] = 20;
+    // s.arr[++s.top] = 30;
+    //  pop operation
+    // printf("Popped element is %d\n", s.arr[s.top--]);
+    // printf("Popped element is %d\n", s.arr[s.top--]);
+    // free(s.arr); // free memory allocated for array
+    struct Stack *s;
+    s->maxSize = 15;
+    s->top = -1;
+    s->arr = (int*)malloc(s->maxSize * sizeof(int)); // allocate memory for given size of array
+    s->arr[++s->top] = 10;
+    s->arr[++s->top] = 20;
+    s->arr[++s->top] = 30;
+    printf("Popped element is %d\n", s->arr[s->top--]);
+    printf("Popped element is %d\n", s->arr[s->top--]);
+    free(s);
+    
      
 return 0;
 }
