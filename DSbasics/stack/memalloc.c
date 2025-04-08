3dc5729aeb263b25854bac54db4e8cf93931bf37
Mohnish <mohnish.a007@gmail.com>
2025-04-08 16:09:42 +0100

    IMP:Very important struct operation  malloc and pointer

--- DSbasics/stack/memalloc.c
+++ DSbasics/stack/memalloc.c
@@ -6,12 +6,19 @@ struct S{
 };
 int main() {
     printf("Understanding Memory alllocation,pointers and different methods \n");
-    printf("Very basic method \n");
-    struct S s1;
-    s1.data1=12;
-    s1.data2=15;
-    printf("The value from struct S instance s1 data1 %d \n",s1.data1);
-    printf("The value from struct S instance s1 data2 %d \n",s1.data2);
+    printf("Using pointer and malloc \n");
+    struct S *sp1 = (struct S *)malloc(sizeof(struct S));
+    sp1->data1 =10;
+    sp1->data2 = 14;
+    printf("The value of data1 using pointer is %d \n",sp1->data1);
+    printf("The value of data2 using pointer is %d \n",sp1->data2);
+
+    // printf("Very basic method \n");
+    // struct S s1;
+    // s1.data1=12;
+    // s1.data2=15;
+    // printf("The value from struct S instance s1 data1 %d \n",s1.data1);
+    // printf("The value from struct S instance s1 data2 %d \n",s1.data2);
 
 return 0;
 }
