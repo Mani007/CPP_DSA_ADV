1625e17d1c2561bdcb24ebd1e09c39d283852a0d
Mohnish <mohnish.a007@gmail.com>
2025-04-10 00:44:20 +0100

    V.IMP: dual pointer challenge

---
+++ DSbasics/stack/bignestedstruct.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node *next;
+};
+struct Bucket {
+    int size;
+    struct Node *N;
+    struct Node *top; // for tracking the top of the bucket something like stack/queue
+};
+int main() {
+    printf("Big nested structs \n");
+    struct Bucket *b = (struct Bucket *) malloc(sizeof(struct Bucket)); // b is pointer to the bucket
+    b->size = 5;
+    b->N = (struct Node *) malloc((b->size)*sizeof(struct Node)); // making array Node and point to first element 
+    b->top = b->N; // making top pointer pointing to first element
+    // Lets insert the elements in Bucket
+    b->N->data = 212;
+    b->N->next = b->N+1;
+    // inserting next element
+    b->N->data = 214;
+    b->N->next = b->N+2;
+    b->top = b->N+1;
+    // inserting third element
+    b->N->data = 216;
+    b->N->next = b->N+3;
+    b->top = b->N+2;
+    // lets do reverse traversal using top pointer
+    for (int i=0;i<3;i++){
+        printf("The value from the top pinter is %d \n",b->(top+i)->data);
+    }
+return 0;
+}
---
+++ DSbasics/stack/functionmalloc.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    printf("The Function for memory allocation and access \n");
+return 0;
+}
