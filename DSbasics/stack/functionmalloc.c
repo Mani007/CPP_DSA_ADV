19446c295b814142acc44cd0470d689d9a0aad96
Mohnish <mohnish.a007@gmail.com>
2025-04-10 02:23:07 +0100

    All basic functions for stack and queue

--- DSbasics/stack/functionmalloc.c
+++ DSbasics/stack/functionmalloc.c
@@ -1,4 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node *next;
+};
+struct Queue {
+    int size;
+    struct Node *N;
+    struct Node *bottom; // for tracking the queue operations
+};
+struct Stack {
+    int size;
+    struct Node *N;
+    struct Node *top; // for tracking the top of the bucket something like stack
+    
+};
+struct Stack * createStack(int size){
+
+}
+struct Queue * createQueue(int size){
+    
+}
+void pushStack(struct Stack *s,int data){
+
+}
+void pushQueue(struct Queue *q,int data){
+
+}
+int popStack(struct Stack *s){
+
+}
+int popQueue(struct Queue *q){
+
+}
+int isEmptyStack(struct Stack *s){
+
+}
+int isEmptyQueue(struct Queue *q){
+
+}
+int isFullStack(struct Stack *s){
+
+}
+int isFullQueue(struct Queue *s){
+
+}
 int main() {
     printf("The Function for memory allocation and access \n");
 return 0;
