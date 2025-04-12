efa16a4413871383088c682aa9a26e53c7968bb2
Mohnish <mohnish.a007@gmail.com>
2025-04-12 22:14:16 +0100

    pushing data using function

--- DSbasics/stack/lltraversal.c
+++ DSbasics/stack/lltraversal.c
@@ -8,12 +8,28 @@
 struct Node {
     int data;
     struct Node *next;
+    struct Node *start;
 
 };
+struct Node * createNode(void){
+    struct Node *N = (struct Node *)malloc(sizeof(struct Node));
+    N->start = N;
+    return N;
+}
+void pushData(struct Node *n,int data){
+    struct Node *N = (struct Node *)malloc(sizeof(struct Node));
+    N->next = NULL;
+    n->data = data;
+    n->next = N;
+}
 
 
 int main() {
     printf("Understding Linked List traversal in dynamic data \n");
-    
+    struct Node *nn = createNode();
+    pushData(nn,25);
+    printf("The data is %d \n",nn->data);
+    pushData(nn,52);
+    printf("The data is %d \n",nn->data);
 return 0;
 }
