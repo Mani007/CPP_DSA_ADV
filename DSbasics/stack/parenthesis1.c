9bbcdaaee7d470ce80e8d1337b2f9c5f57fcd073
Mohnish <mohnish.a007@gmail.com>
2025-04-15 15:49:03 +0100

    basic char array printed char by char

--- DSbasics/stack/parenthesis1.c
+++ DSbasics/stack/parenthesis1.c
@@ -67,16 +67,9 @@ int parenthesisMatch(char *exp){
 }
 int main() {
     printf("Parenthesis matching \n");
-    char c[5];
-    char *cp = c;
-    *cp = 'a';
-    *(cp+1) = 'b';
-    printf("Char is %c \n",*cp);
-    printf("Char is %c \n",*(cp+1));
-    printf("Char is %c \n",*(cp+2)); // some garbage value is printer in the array
-    printf("Char is %c \n",*(cp+4)); // some garbage value is printer in the array
-    printf("Char is %c \n",*(cp+5)); // some garbage value is printer in the array
-    printf("Char is %c \n",*(cp+6)); // some garbage value is printer in the array
+    char *cp = "abcdef";
+    printf("The char array is %c \n",*cp); // this will only print the first character of the array
+    printf("The char array is %c \n",*(cp+1)); // this will only print the second character of the array
     // struct Node *C=createStack(10);
     // C = pushNode(C,'a');
     // C = pushNode(C,'b');
