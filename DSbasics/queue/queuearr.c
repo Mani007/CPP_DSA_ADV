d52e8e1fdbf9bf24daf8436ebbfffa0056f1beed
Mohnish <mohnish.a007@gmail.com>
2025-04-16 15:06:51 +0100

    basic queue operations working fine

--- DSbasics/queue/queuearr.c
+++ DSbasics/queue/queuearr.c
@@ -7,29 +7,71 @@ struct Queue {
     int last;
     int *arr;
 };
+struct Queue * createQueue(int size){
+    struct Queue *qq = (struct Queue *)malloc(sizeof(struct Queue));
+    qq->size = size;
+    qq->front = -1;
+    qq->last = -1;
+    qq->arr = (int *)malloc(size*sizeof(int));
+    qq->front+=1;
+    return qq;
+}
+int qEmpty(struct Queue *qq){
+    if (qq->front == -1 && qq->last == -1){
+        printf("The queue is Empty. \n");
+        return 1;
+    }else {
+        return 0;
+    }
+}
+int qFull(struct Queue *qq){
+    if (qq->front == qq->size-1){
+        printf("Queue is completely full. \n");
+        return 1;
+    } else {
+        return 0;
+    }
+}
+struct Queue * qpush(struct Queue *qq,int data){
+    // check if FULL and create the node for LL implementation 
+    //for arrays just use the qq pointer
+    if (qFull(qq)==1){
+        printf("The queue is packed. \n");
+        return qq;
+    } else {
+        //(qq->arr)+1 = data; // This syntax is for linked list
+        qq->last+=1;
+        qq->arr[qq->last] = data;
+        return qq;
+    }
+}
+struct Queue * qpop(struct Queue *qq){
+    if (qEmpty(qq)==1){
+        printf("Queue is Empty. \n");
+        return qq;
+    } else {
+        qq->last -=1;
+        return qq;
+    }
+}
+void display(struct Queue *qq){
+    if (qEmpty(qq)==1){
+        printf("Queue is Empty. \n");
+    } else {
+        for (int i = qq->front;i<=qq->last;i++){
+            printf("The data in index %d is %d \n",i,qq->arr[qq->last-i]);
+        }
+    }
+}
 int main() {
     printf("Queue data structure using ARRAY \n");
     // initiating the queue structure
-    struct Queue *qq = (struct Queue *)malloc(sizeof(struct Queue));
-    qq->size = 10;
-    qq->front = -1;
-    qq->last = -1;
-    qq->arr = (int *)malloc(qq->size*sizeof(int));
-    // adding value in queue
-    qq->arr[0] = 25;
-    qq->front +=1;
-    qq->last +=1;
-    // second value 
-    qq->arr[1] = 45;
-    //qq->front +=1;
-    qq->last +=1;
-    // third element 
-    qq->arr[2] = 55;
-    //qq->front +=1;
-    qq->last +=1;
-    // print all the element from the queue 
-    for (int i=qq->front;i<=qq->last;i++){
-        printf("The value of element at %d is %d \n",i,qq->arr[i]);
-    }
+    struct Queue *q = createQueue(7);
+    q = qpush(q,25); 
+    q = qpush(q,45); 
+    q = qpush(q,55); 
+    q = qpush(q,65);
+    display(q); 
+    
 return 0;
 }
