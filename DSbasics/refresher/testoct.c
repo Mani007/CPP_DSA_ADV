950c37891381d79f99098c718d8df21d9611491a
Mohnish <mohnish.a007@gmail.com>
2025-10-06 19:17:02 +0100

    basic refresher code - struct without pointer

--- DSbasics/refresher/testoct.c
+++ DSbasics/refresher/testoct.c
@@ -15,39 +15,22 @@
 //         printf("Element in index %d of array are %d \n",i,arr[i]);
 //     }
 // }
+struct data{
+    int index;
+    int d;
+};
+
 int main() {
 
-    // insertdata(arr1, 0, 10, 5);
-    // insertdata(arr1, 1, 20, 5);
-    // insertdata(arr1, 2, 30, 5);
-    // insertdata(arr1, 3, 40, 5);
-    // insertdata(arr1, 4, 50, 5);
-    // displayarr(arr1,5);
-    // putting elements in array
-    // arr[1] = 10;
-    // arr[2] = 20;
-    // arr[3] = 30;
-    // arr[4] = 40;
-    // arr[5] = 50;
-    // display element in array
-    // int i =0;
-    // for(i=0;i<=5;i++){
-    //     printf("Element in index %d of array are %d \n",i,arr[i]);
-    // }
     printf("Testing the programming skills\n");
-    printf("Implementing array and pointer in C\n");
-    int arr1[5]; // integer array of size 5
-    int *ptr[] = &arr1; // this will represent pointer to ENTIRE array Wrong way
-    // *ptr = 10;
-    // *(ptr+1) = 20;
-    // *(ptr+2) = 30;
-    // *(ptr+3) = 40;
-    // *(ptr+4) = 50;
-    // int i;
-    // for(i = 0; i<5; i++){
-
-    //     printf("Element in index %d  at address %p of array are %d \n",i,(ptr+i),*(ptr + i));
-    // }
-
+    printf("Implementing Structure and pointer in C\n");
+    struct data d1;
+    struct data d2;
+    d1.index = 1;
+    d2.index = 2;
+    d1.d = 10;
+    d2.d = 20;
+    printf("Index of d1 is %d and data is %d \n",d1.index,d1.d);
+    printf("Index of d2 is %d and data is %d \n",d2.index,d2.d);
 return 0;
 }
