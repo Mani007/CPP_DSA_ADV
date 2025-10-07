39805115e6699d20346735b036028ab1ba99ddc8
Mohnish <mohnish.a007@gmail.com>
2025-10-07 15:25:13 +0100

    basic refresher code - struct with malloc

--- DSbasics/refresher/testoct.c
+++ DSbasics/refresher/testoct.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 // void insertdata(int arr[], int index, int data, int length){
 //     //int len = sizeof(arr)/sizeof(arr[0]);
 //     int len = length;
@@ -15,27 +16,16 @@
 //         printf("Element in index %d of array are %d \n",i,arr[i]);
 //     }
 // }
-struct data{
-    int index;
+struct Data{
     int d;
     struct data *next; // for storing address of next node
-};
+} data;
 
 int main() {
 
     printf("Testing the programming skills\n");
-    printf("Implementing Structure and pointer in C\n");
-    struct data d1;
-    struct data *p1;
-    p1 = &d1;
-    p1->index = 1;
-    p1->d = 10;
-    struct data d2;
-    struct data *p2;
-    p2 = &d2;
-    p2->index = 2;
-    p2->d = 20;
-    printf("Using pointer Index of d1 is %d and data is %d \n",p1->index,p1->d);
-    printf("Using pointer Index of d2 is %d and data is %d \n",p2->index,p2->d);
+    printf("Implementing dynamic list and pointer in C\n");
+    struct Data *ptr = malloc(sizeof(struct Data));
+    
 return 0;
 }
