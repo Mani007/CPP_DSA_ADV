b0bed1f9eab8944793d61df2b8b0795b7c185038
Mohnish <mohnish.a007@gmail.com>
2025-10-15 02:13:53 +0100

    basic refresher code - stack exaple need some more work

--- DSbasics/refresher/testds.c
+++ DSbasics/refresher/testds.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 // Function needed for the array are push, pop, isFull, isEmpty
-void push(int a[], int top, int val, int last){
-    if (top<last){
-        a[top] = val;
-        top = top + 1;
+// pass by value function look like void push(void), void pop(void) etc. 
+// pass by reference 
+void push(int *a[], int *top, int val, int *last){
+    if (*(top)<=*(last)){
+        a[*(top)] = val;
+        top = top+1;
     } else {
         printf("The Stack is full");
     }
@@ -11,12 +13,15 @@ void push(int a[], int top, int val, int last){
 int main() {
     printf("Testing skill for DS - Stack and queue. \n");
     printf("Implement stack using array. \n");
-    int top = 0;
     int last = 5;
-    int arr[]= {0,0,0,0,0};
-    push(arr,top,10,last);
-    printf("Top is %d \n", top);
-    printf("last is %d \n", last);
-    printf("Element added is %d \n", arr[top]);
+    int *l = &last;
+    int top = 0;
+    int *t = &top;
+    int* arr[5];
+    push(arr,t,20,l);
+    push(arr,t,30,l);
+    printf("Value at index 0 is %d \n", *arr);
+    printf("Value at index 1 is %d \n", *arr+1);
+
 return 0;
 }
